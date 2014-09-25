4637a4fd506d0d48c1d3363a27e16d0b15ddd863
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2014-09-25 16:20:03 +0100

    update url

--- cookbooks/windows_slave/attributes/default.rb
+++ cookbooks/windows_slave/attributes/default.rb
@@ -7,17 +7,17 @@
 # All rights reserved - Do Not Redistribute
 #
 
-default['windows_slave']['extra_pakages'] = {}
-default['windows_slave']['jre'] = "C:\\\\Program Files\\\\Java\\\\jre7\\\\bin\\\\java"
-default['windows_slave']['wget'] = "wget"
-default['windows_slave']['Teamcity']['enable'] = false
-default['windows_slave']['Teamcity']['serverurl'] = 'http://localhost:8111'
+default['windows_slave']['extra_pakages']          = {}
+default['windows_slave']['jre']                    = "C:\\\\Program Files\\\\Java\\\\jre7\\\\bin\\\\java"
+default['windows_slave']['wget']                   = "wget"
+default['windows_slave']['Teamcity']['enable']     = false
+default['windows_slave']['Teamcity']['serverurl']  = 'http://localhost:8111'
 default['windows_slave']['Teamcity']['local_path'] = 'Z:\\TeamCity'
-default['windows_slave']['pickles'] = 'https://github.com/picklesdoc/pickles/releases/download/v0.17.4/pickles-0.17.4.zip'
-default['windows_slave']['opencover'] = 'https://github.com/OpenCover/opencover/releases/download/4.5.2506/opencover.4.5.2506.msi'
+default['windows_slave']['pickles']              = 'https://github.com/picklesdoc/pickles/releases/download/v0.18.2/Pickles-0.18.2.zip'
+default['windows_slave']['opencover']            = 'https://github.com/OpenCover/opencover/releases/download/4.5.2506/opencover.4.5.2506.msi'
 default['windows_slave']['jenkins']['master_ip'] = '10.210.194.249'
-default['windows_slave']['jenkins']['username'] = 'changeme'
-default['windows_slave']['jenkins']['password'] = 'changeme'
-default['windows_slave']['sonar_version'] = '2.4'
-default['windows_slave']['resharper'] = 'http://download-cf.jetbrains.com/resharper/jb-commandline-8.2.1000.4527.zip'
-default['windows_slave']['choco_path'] = 'C:/ProgramData/chocolatey/bin'
+default['windows_slave']['jenkins']['username']  = 'changeme'
+default['windows_slave']['jenkins']['password']  = 'changeme'
+default['windows_slave']['sonar_version']        = '2.4'
+default['windows_slave']['resharper']            = 'http://download-cf.jetbrains.com/resharper/jb-commandline-8.2.1000.4527.zip'
+default['windows_slave']['choco_path']           = 'C:/ProgramData/chocolatey/bin'
